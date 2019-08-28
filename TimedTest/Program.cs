5477056088052301c6df808b97730cf17d0d1d7d
Chris Gardner <cgardner@gmail.com>
2019-08-28 13:58:52 -0500

    Randomly changed order of terms (so larger value wouldn't always be on top.)

--- TimedTest/Program.cs
+++ TimedTest/Program.cs
@@ -7,21 +7,25 @@ using System.Threading.Tasks;
 
 namespace TimedTest {
 	class Program {
+		static Random random = new Random();
+
 		static IEnumerable<(int top, int bottom)> GetPossibleValues() {
 			foreach( int top in Enumerable.Range( 0, 10 ) )
 				foreach( int bottom in Enumerable.Range( 0, top + 1 ) )
-					yield return (top, bottom);
+					if( 0.5 > random.NextDouble() )
+						yield return (top, bottom);
+					else
+						yield return (bottom, top);
 		}
 
 		static void Main( string[] args ) {
-			IEnumerable<(int top, int bottom)> possibleValues = GetPossibleValues();
+			List<(int top, int bottom)> possibleValues = GetPossibleValues().ToList();
 			IEnumerable<(int top, int bottom)> chosenValues = Enumerable.Empty<(int top, int bottom)>();
 
-			Random random = new Random();
 			while( possibleValues.Any() ) {
 				int index = random.Next( possibleValues.Count() );
 				chosenValues = chosenValues.Append( possibleValues.Skip( index ).First() );
-				possibleValues = possibleValues.Except( new[] { possibleValues.ElementAt( index ) } );
+				possibleValues.RemoveAt( index );
 			}
 
 			int rows = 7;

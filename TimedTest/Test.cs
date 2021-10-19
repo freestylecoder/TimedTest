181dba2885fafd45ae4898d36f69138fae0d62fb
Chris Gardner <cgardner@gmail.com>
2021-10-19 11:59:28 -0500

    I dunno, but I need to check it in...

--- TimedTest/Test.cs
+++ TimedTest/Test.cs
@@ -11,33 +11,48 @@ namespace TimedTest {
 		public Test( int seed ) {
 			random = new Random( seed );
 
-			testQuestions = RandomizeList( Enumerable.Range( 0, 55 ) )
-				.Select( i => Enumerable
-					.Range( 0, 10 )
-					.SelectMany( top =>
-						Enumerable
-							.Range( 0, top + 1 )
-							.Select( bottom =>
-								0.5 > random.NextDouble()
-								? (top, bottom)
-								: (bottom, top)
-							)
+			IEnumerable<(int, int)> x = Enumerable
+				.Range( 0, 10 )
+				.SelectMany( top =>
+					Enumerable
+						.Range( 0, top + 1 )
+						.Select( bottom =>
+							0.5 > random.NextDouble()
+							? (top, bottom)
+							: (bottom, top)
 						)
+				)
+				.ToList();
+
+			testQuestions = RandomizeList( Enumerable.Range( 0, 55 ) )
+				.Select( i => x
 					.Skip( i )
 					.First()
 				);
 		}
 
 		private IEnumerable<int> RandomizeList( IEnumerable<int> list ) {
-			if( null == list ) return Enumerable.Empty<int>();
-			if( !list.Any() ) return list;
+			List<int> newList = new List<int>();
+			while( list.Any() ) {
+				int index = random.Next( 0, list.Count() );
+				newList.Add( list.ElementAt( index ) );
+				list = list.Where( ( item, ind ) => ind != index ).ToList();
+			}
 
-			int index = random.Next( list.Count() );
+			return newList;
+			//for( int index = 0; index < list.Count(); ++index ) {
+			//	list = list.Take( index )
+			//		.Concat( random.Next( index, list.Count() ) )
+			//}
+			//if( null == list ) return Enumerable.Empty<int>();
+			//if( !list.Any() ) return list;
 
-			return RandomizeList(
-					list.Take( index ).Concat( list.Skip( index + 1 ) )
-				)
-				.Prepend( list.Skip( index ).First() );
+			//int index = random.Next( list.Count() );
+
+			//return RandomizeList(
+			//		list.Take( index ).Concat( list.Skip( index + 1 ) )
+			//	)
+			//	.Prepend( list.Skip( index ).First() );
 		}
 
 		#region IEnumerable<(int top, int bottom)>
